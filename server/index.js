2f5cd790f8fa378c3b8f9890207df73934929e04
jiacheng1216 <ah881216@gmail.com>
2025-02-28 21:41:48 +0800

    feat:index.js server入口

---
+++ server/index.js
@@ -0,0 +1,20 @@
+const express = require("express");
+const app = express();
+const mongoose = require("mongoose");
+dotenv.config();
+
+mongoose
+  .connect("mongodb://localhost:27017/stoneDB")
+  .then(() => {
+    console.log("Connected to stoneDB");
+  })
+  .catch((e) => {
+    console.log(e);
+  });
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+app.listen(8080, () => {
+  console.log("Server is running on port 8080");
+});
