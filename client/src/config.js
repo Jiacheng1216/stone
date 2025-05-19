c1c80d9cb77382794b4a95336ec245c2f99aeab2
Eric.Lin <Eric.Lin@skylinetw.com>
2025-05-19 15:08:20 +0800

    fix：修改後端網域

--- client/src/config.js
+++ client/src/config.js
@@ -1,1 +1,3 @@
-export const ip = "https://shiningstone-c1330af9cdf1.herokuapp.com";
+export const ip = "https://shiningstoneserver.onrender.com";
+// export const ip = "https://shiningstone-c1330af9cdf1.herokuapp.com";
+// export const ip = "http://127.0.0.1:8080";
